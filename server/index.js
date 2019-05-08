c7256b04303f14e3fa22154ab0c732c5c52cb483
Reza Lotfi <reza.lotfi14@gmail.com>
2019-05-08 09:32:08 -0700

    Begin working on implementing server paths

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var axios = require('axios');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var items = require('../database-mongo');
 
 var app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
@@ -22,6 +23,14 @@ app.get('/items', function (req, res) {
     }
   });
 });
+
+app.get('/places', (req, res) => {
+  axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=[API_KEY_HERE]')
+    .then(response => {
+      console.log(response)
+      res.send(response)
+    })
+})
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
